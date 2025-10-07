605fd2594cfbc1dadc7cff5a31968db1b1fbef8d
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2025-10-07 21:46:27 +0200

    generate immich app

--- k3s/definitions/immich.nix
+++ k3s/definitions/immich.nix
@@ -1,8 +1,37 @@
-{ ... }:
+{ lib, ... }:
 {
   applications.immich = {
     namespace = "immich";
     createNamespace = true;
+
+    helm.releases.immich = {
+      chart = lib.helm.downloadHelmChart {
+        repo = "oci://ghcr.io/immich-app/immich-charts";
+        chart = "immich";
+        version = "0.9.3";
+        chartHash = "sha256-UHuuu6u+UjHPgdLONZim6j+nyCINtClcAZRRJlHuaaw=";
+      };
+
+      values = {
+        image.tag = "v1.139.3";
+        immich = {
+          persistence.library.existingClaim = "pvc-immich-rw";
+        };
+        valkey = {
+          enabled = true;
+          persistence.data = {
+            type = "pvc";
+            storageClass = "longhorn";
+          };
+        };
+        machine-learning = {
+          persistence.cache = {
+            type = "pvc";
+            storageClass = "longhorn";
+          };
+        };
+      };
+    };
 
     resources = {
       # PG Cluster
@@ -21,28 +50,25 @@
         enableSuperuserAccess = true;
       };
 
-      persistentVolumes.pv-photos-ro = {
-        metadata.annotations = {
-          "pv.kubernetes.io/provisioned-by" = "smb.csi.k8s.io";
-        };
-        spec = {
-          capacity.storage = "10Gi";
-          accessModes = [ "ReadOnlyMany" ];
-          storageClassName = "smb";
-          mountOptions = [
-            "dir_mode=0777"
-            "file_mode=0444"
+      # Patch generated resources
+      deployments.immich-server.spec.template.spec = {
+        containers.immich-server = {
+          env.DB_URL_FILE.value = "/etc/secret/fqdn-uri";
+          volumeMounts = [
+            {
+              name = "photos";
+              readOnly = true;
+              mountPath = "/mnt/media/rodina";
+            }
+            {
+              name = "dburl";
+              readOnly = true;
+              mountPath = "/etc/secret";
+            }
           ];
-          csi = {
-            driver = "smb.csi.k8s.io";
-            volumeHandle = "10.42.0.3/smb_photos_ro";
-            volumeAttributes.source = "//10.42.0.3/photos";
-            nodeStageSecretRef = {
-              name = "smb-photos-ro";
-              namespace = "immich";
-            };
-          };
         };
+        volumes.photos.persistentVolumeClaim.claimName = "pvc-photos-ro";
+        volumes.dburl.secret.secretName = "immich-database-superuser";
       };
 
       roles.csi-smb-secret-access.rules = [
@@ -71,11 +97,95 @@
         };
       };
 
+      persistentVolumes.pv-photos-ro = {
+        metadata.annotations = {
+          "pv.kubernetes.io/provisioned-by" = "smb.csi.k8s.io";
+        };
+        spec = {
+          capacity.storage = "10Gi";
+          accessModes = [ "ReadOnlyMany" ];
+          storageClassName = "smb";
+          mountOptions = [
+            "dir_mode=0777"
+            "file_mode=0444"
+          ];
+          csi = {
+            driver = "smb.csi.k8s.io";
+            volumeHandle = "10.42.0.3/smb_photos_ro";
+            volumeAttributes.source = "//10.42.0.3/photos";
+            nodeStageSecretRef = {
+              name = "smb-photos-ro";
+              namespace = "immich";
+            };
+          };
+        };
+      };
+
       persistentVolumeClaims.pvc-photos-ro.spec = {
         accessModes = [ "ReadOnlyMany" ];
         volumeName = "pv-photos-ro";
         storageClassName = "smb";
         resources.requests.storage = "10Gi";
+      };
+
+      persistentVolumes.pv-immich-rw = {
+        metadata.annotations = {
+          "pv.kubernetes.io/provisioned-by" = "smb.csi.k8s.io";
+        };
+        spec = {
+          capacity.storage = "2Ti";
+          accessModes = [ "ReadWriteOnce" ];
+          storageClassName = "smb";
+          mountOptions = [
+            "dir_mode=0777"
+            "file_mode=0444"
+          ];
+          csi = {
+            driver = "smb.csi.k8s.io";
+            volumeHandle = "10.42.0.3/smb_immich_rw";
+            volumeAttributes.source = "//10.42.0.3/photos/immich";
+            nodeStageSecretRef = {
+              name = "smb-immich-rw";
+              namespace = "immich";
+            };
+          };
+        };
+      };
+
+      persistentVolumeClaims.pvc-immich-rw.spec = {
+        accessModes = [ "ReadWriteOnce" ];
+        volumeName = "pv-immich-rw";
+        storageClassName = "smb";
+        resources.requests.storage = "2Ti";
+      };
+
+      ingressRoutes.immich.spec = {
+        entryPoints = [ "websecure" ];
+        routes = [
+          {
+            match = "Host(`immich.doma.lol`)";
+            kind = "Rule";
+            services.http.port = 80;
+          }
+        ];
+        tls = {
+          certResolver = "letsencrypt";
+          domains = [
+            {
+              main = "doma.lol";
+              sans = [ "*.doma.lol" ];
+            }
+          ];
+        };
+      };
+
+      ingresses.immich.spec = {
+        ingressClassName = "traefik";
+        rules = [
+          {
+            host = "immich.doma.lol";
+          }
+        ];
       };
     };
   };
