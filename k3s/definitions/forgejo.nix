431160302fd96e431b911c2cacd7867133f1a129
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2025-10-08 21:20:27 +0200

    add forgejo runner

--- k3s/definitions/forgejo.nix
+++ k3s/definitions/forgejo.nix
@@ -27,6 +27,167 @@
     };
 
     resources = {
+      # Runners
+      deployments.forgejo-runner.spec = {
+        replicas = 1;
+        selector.matchLabels.app = "forgejo-runner";
+        template = {
+          metadata.labels.app = "forgejo-runner";
+          spec = {
+            automountServiceAccountToken = true;
+            restartPolicy = "Always";
+            initContainers = [
+              {
+                name = "runner-register";
+
+                image = "code.forgejo.org/forgejo/runner:6.4.0";
+                command = [
+                  "/bin/bash"
+                  "-c"
+                ];
+                args = [
+                  ''
+                    while : ; do
+                      forgejo-runner register --no-interactive --token $(RUNNER_SECRET) --name $(RUNNER_NAME) --instance $(FORGEJO_INSTANCE_URL) && break ;
+                      sleep 1 ;
+                    done ;
+                    forgejo-runner generate-config > /data/config.yml ;
+                    sed -i -e "s|network: .*|network: host|" config.yml ;
+                    sed -i -e "s|^  envs:$$|  envs:\n    DOCKER_HOST: tcp://localhost:2376\n    DOCKER_TLS_VERIFY: 1\n    DOCKER_CERT_PATH: /certs/client|" config.yml ;
+                    sed -i -e "s|^  options:|  options: -v /certs/client:/certs/client|" config.yml ;
+                    sed -i -e "s|  valid_volumes: \[\]$$|  valid_volumes:\n    - /certs/client|" config.yml
+                  ''
+                ];
+                env = [
+                  {
+                    name = "RUNNER_NAME";
+                    valueFrom.fieldRef.fieldPath = "metadata.name";
+                  }
+                  {
+                    name = "RUNNER_SECRET";
+                    valueFrom.secretKeyRef = {
+                      name = "forgejo-runner";
+                      key = "token";
+                    };
+                  }
+                  {
+                    name = "FORGEJO_INSTANCE_URL";
+                    value = "http://forgejo-http.svc.cluster.local:3000";
+                  }
+                ];
+                volumeMounts = [
+                  {
+                    name = "runner-data";
+                    mountPath = "/data";
+                  }
+                ];
+                securityContext = {
+                  allowPrivilegeEscalation = false;
+                  capabilities.drop = [ "ALL" ];
+                  privileged = false;
+                  readOnlyRootFilesystem = true;
+                  runAsNonRoot = true;
+                  seccompProfile.type = "RuntimeDefault";
+                };
+              }
+            ];
+            containers = {
+              runner = {
+                image = "code.forgejo.org/forgejo/runner:6.4.0";
+                command = [
+                  "/bin/bash"
+                  "-c"
+                ];
+
+                args = [
+                  ''
+                    while ! nc -z localhost 2376 </dev/null ; do
+                      echo 'waiting for docker daemon...' ;
+                      sleep 5 ;
+                      done ;
+                    forgejo-runner --config config.yml daemon
+                  ''
+                ];
+                env = [
+                  {
+                    name = "DOCKER_HOST";
+                    value = "tcp://localhost:2376";
+                  }
+                  {
+                    name = "DOCKER_CERT_PATH";
+                    value = "/certs/client";
+                  }
+                  {
+                    name = "DOCKER_TLS_VERIFY";
+                    value = "1";
+                  }
+
+                ];
+                volumeMounts = [
+                  {
+                    name = "docker-certs";
+                    mountPath = "/certs";
+
+                  }
+                  {
+                    name = "runner-data";
+                    mountPath = "/data";
+
+                  }
+                  {
+                    name = "tmp";
+                    mountPath = "/tmp";
+
+                  }
+                ];
+                securityContext = {
+                  allowPrivilegeEscalation = false;
+                  capabilities.drop = [
+                    "ALL"
+                  ];
+                  privileged = false;
+                  readOnlyRootFilesystem = true;
+                  runAsNonRoot = true;
+                  seccompProfile.type = "RuntimeDefault";
+                };
+              };
+              daemon = {
+                image = "docker.io/docker:28.3.0-dind";
+                env = [
+                  {
+                    name = "DOCKER_TLS_CERTDIR";
+                    value = "/certs";
+                  }
+                ];
+                securityContext.privileged = true;
+                volumeMounts = [
+                  {
+                    name = "docker-certs";
+                    mountPath = "/certs";
+
+                  }
+                ];
+              };
+            };
+            volumes = [
+              {
+                name = "docker-certs";
+                emptyDir = { };
+              }
+              {
+                name = "runner-data";
+                emptyDir = { };
+              }
+              {
+                name = "tmp";
+                emptyDir = { };
+              }
+            ];
+          };
+        };
+      };
+
+      # Ingress
       ingressRoutes.forgejo.spec = {
         entryPoints = [ "websecure" ];
         routes = [
