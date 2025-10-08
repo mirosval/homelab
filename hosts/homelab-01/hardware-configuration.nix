8389c9588515fcafa137654ffbc09ce3ea2bfd2b
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2025-10-08 21:53:41 +0200

    increase fs.inotify.max_user_instances

--- hosts/homelab-01/hardware-configuration.nix
+++ hosts/homelab-01/hardware-configuration.nix
@@ -27,6 +27,10 @@
     initrd.kernelModules = [ ];
     kernelModules = [ "kvm-intel" ];
     extraModulePackages = [ ];
+    kernel.sysctl = {
+      "fs.inotify.max_user_instances" = 1024;
+      "fs.inotify.max_user_watches" = 524288;
+    };
   };
 
   fileSystems = {
