b36eb4d54a67bd894a285b12e91e665c1d20e9a5
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2025-10-10 17:26:01 +0200

    fix coredns

--- k3s/definitions/coredns-config.nix
+++ k3s/definitions/coredns-config.nix
@@ -6,7 +6,8 @@
     # This tells coredns to rewrite all dns queries from within the cluster
     # that would have gone to *.doma.lol to go directly to the Traefik instead
     resources.configMaps.coredns-custom.data = {
-      "rewritedomalol.override" = "rewrite name regex (.*)\.doma\.lol traefik.traefik.svc.cluster.local.";
+      "rewritedomalol.override" =
+        "rewrite name regex (.*)\\.doma\\.lol traefik.traefik.svc.cluster.local.";
     };
   };
 }
