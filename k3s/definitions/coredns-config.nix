0cc5e0ecb5c15209dcc5c29fbebe59ef0836a1c4
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2025-10-10 16:39:56 +0200

    comment coredns custom

--- k3s/definitions/coredns-config.nix
+++ k3s/definitions/coredns-config.nix
@@ -3,6 +3,8 @@
   applications.coredns-config = {
     namespace = "kube-system";
     createNamespace = false;
+    # This tells coredns to rewrite all dns queries from within the cluster
+    # that would have gone to *.doma.lol to go directly to the Traefik instead
     resources.configMaps.coredns-custom.data = {
       "rewritedomalol.override" =
         "rewrite name rewrite (.*)\.doma\.lol traefik.traefik.svc.cluster.local.";
