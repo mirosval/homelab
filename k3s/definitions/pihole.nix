2a37b12e84ac95dea939e8451b0edc9e1c74ef73
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2026-03-18 19:36:27 +0100

    update pihole to v2.35.0

--- k3s/definitions/pihole.nix
+++ k3s/definitions/pihole.nix
@@ -8,7 +8,7 @@
         repo = "https://mojo2600.github.io/pihole-kubernetes/";
         chart = "pihole";
         version = "2.35.0";
-        chartHash = "sha256-nhvifpDdM8MoxF43cJAi6o+il2BbHX+udVAvvm1PukM=";
+        chartHash = "sha256-wWFj3/2BsiQMXcAoG8buJRWUXkcKS6Ies1veUtMcHYc=";
       };
 
       values = {
